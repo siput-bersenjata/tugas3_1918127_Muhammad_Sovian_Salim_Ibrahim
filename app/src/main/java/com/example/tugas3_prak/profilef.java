4cd76c8fe10055dc630f1f91ac7cacdb69c5d4d1
unknown <bramgitaris@gmail.com>
2021-10-18 19:24:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas3_prak/profilef.java
@@ -0,0 +1,22 @@
+package com.example.tugas3_prak;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class profilef extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle
+                                     savedInstanceState) {
+        return
+                inflater.inflate(R.layout.f_profil,container,false);
+    }
+}
+
